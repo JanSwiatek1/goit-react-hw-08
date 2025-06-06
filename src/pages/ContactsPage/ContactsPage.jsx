1b1020f32cc027e0d8c4812304d8c78fe413e8d4
Jan Świątek <swiatek-jan@wp.pl>
2025-06-07 00:23:36 +0200

    s

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import ContactForm from '../../components/ContactForm/ContactForm';
+import ContactForm from '../../components/ContactForm/ContactForm.jsx';
 import ContactList from '../../components/ContactList/ContactList';
 import SearchBox from '../../components/SearchBox/SearchBox';
 import { fetchContacts } from '../../redux/contacts/contactsOperations';
