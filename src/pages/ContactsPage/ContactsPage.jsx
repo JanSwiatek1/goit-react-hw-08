1eeb4bd435435803e273475adc9e7f2738ac498a
Jan Świątek <swiatek-jan@wp.pl>
2025-06-07 00:56:46 +0200

    test9

--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import ContactForm from '../../components/ContactForm';  
 import ContactList from '../../components/ContactList'; 
-import SearchBox from '../../components/SearchBox/SearchBox';
+import SearchBox from '../../components/SearchBox';
 import { fetchContacts } from '../../redux/contacts/operations';  
 
 export default function ContactsPage() {
