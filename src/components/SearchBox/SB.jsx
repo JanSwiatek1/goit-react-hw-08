7352c98726dfbe7715b40cfb6b0476c51cadbcc2
Jan Świątek <swiatek-jan@wp.pl>
2025-06-07 22:31:47 +0200

    SB

---
+++ src/components/SearchBox/SB.jsx
@@ -0,0 +1,19 @@
+import { useDispatch } from 'react-redux';
+import { changeFilter } from '../../redux/filtersSlice';
+
+
+export default function SearchBox() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <p>Find contacts by name</p>
+      <input
+        type="text"
+        onChange={(e) => dispatch(changeFilter(e.target.value))}
+      />
+    </div>
+  );
+}
+
+// export default SearchBox;
--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import ContactForm from '../../components/ContactForm/CForm';  
 import ContactList from '../../components/ContactList/CList'; 
-import SearchBox from '../../components/SearchBox';
+import SearchBox from '../../components/SearchBox/';
 import { fetchContacts } from '../../redux/contacts/operations';  
 
 export default function ContactsPage() {
