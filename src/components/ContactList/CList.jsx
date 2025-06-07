36b36dfb60240fc1acdc2fbf41c75344662ae53a
Jan Świątek <swiatek-jan@wp.pl>
2025-06-07 22:12:29 +0200

    CList

---
+++ src/components/ContactList/CList.jsx
@@ -0,0 +1,37 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContacts, deleteContact } from '../../redux/contacts/operations';
+import { selectNameFilter } from '../../redux/filtersSlice';
+import { selectContacts, selectIsLoading } from '../../redux/contacts/selectors';
+import Contact from '../Contact/Contact';
+import css from './ContactList.module.css';
+
+export default function ContactList() {
+  const dispatch = useDispatch();
+  const contacts = useSelector(selectContacts);
+  const isLoading = useSelector(selectIsLoading);
+  const filter = useSelector(selectNameFilter);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  const filteredContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase())
+  );
+
+  if (isLoading) return <p>Loading contacts...</p>;
+  if (!contacts.length) return <p>Your phonebook is empty. Add first contact!</p>;
+
+  return (
+    <ul className={css.list}>
+      {filteredContacts.map(contact => (
+        <li key={contact.id} className={css.item}>
+          <Contact contact={contact} onDelete={() => dispatch(deleteContact(contact.id))} />
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+// export default ContactList; 
--- src/pages/ContactsPage/ContactsPage.jsx
+++ src/pages/ContactsPage/ContactsPage.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import ContactForm from '../../components/ContactForm/CForm';  
-import ContactList from '../../components/ContactList'; 
+import ContactList from '../../components/ContactList/CList'; 
 import SearchBox from '../../components/SearchBox';
 import { fetchContacts } from '../../redux/contacts/operations';  
 
